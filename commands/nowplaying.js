f267bc908079ec9088b474b89012d3208889d57a
MakiiMjuza <78150396+MakiiMjuza@users.noreply.github.com>
2021-03-15 11:53:43 +0100

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,43 +1,25 @@
-const createBar = require("string-progressbar");
-const { MessageEmbed } = require("discord.js");
+/* eslint linebreak-style: 0 */
+const Command = require('../util/Command.js');
 
-const { LOCALE } = require("../util/EvobotUtil");
-const i18n = require("i18n");
-
-i18n.setLocale(LOCALE);
-
-module.exports = {
-  name: "np",
-  description: i18n.__("nowplaying.description"),
-  execute(message) {
-    const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply(i18n.__("nowplaying.errorNotQueue")).catch(console.error);
-
-    const song = queue.songs[0];
-    const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
-    const left = song.duration - seek;
-
-    let nowPlaying = new MessageEmbed()
-      .setTitle(i18n.__("nowplaying.embedTitle"))
-      .setDescription(`${song.title}\n${song.url}`)
-      .setColor("#F8AA2A")
-      .setAuthor(message.client.user.username);
-
-    if (song.duration > 0) {
-      nowPlaying.addField(
-        "\u200b",
-        new Date(seek * 1000).toISOString().substr(11, 8) +
-          "[" +
-          createBar(song.duration == 0 ? seek : song.duration, seek, 20)[0] +
-          "]" +
-          (song.duration == 0 ? " ◉ LIVE" : new Date(song.duration * 1000).toISOString().substr(11, 8)),
-        false
-      );
-      nowPlaying.setFooter(
-        i18n.__mf("nowplaying.timeRemaining", { time: new Date(left * 1000).toISOString().substr(11, 8) })
-      );
-    }
-
-    return message.channel.send(nowPlaying);
+class NP extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'ts',
+      description: 'Ova naredba će prikazati trenutno reproduciranu pjesmu.',
+      usage: 'np',
+      aliases: ['nowplaying', 'current'],
+      cooldown: 5,
+      category: 'Music'
+    });
   }
-};
+
+  async run(message) {
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) return this.client.embed('nijeGlasovnikanal', message);
+    if (!this.client.playlists.has(message.guild.id)) return this.client.embed('prazanQue', message);
+    return this.client.embed('trenutmoSvira', message);
+  }
+} 
+
+module.exports = NP;
+
