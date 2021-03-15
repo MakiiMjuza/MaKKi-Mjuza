f4893e0f3ffef2cdce85699cbf40ea7c2955a17b
MakiiMjuza <78150396+MakiiMjuza@users.noreply.github.com>
2021-03-15 12:05:25 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,65 +1,55 @@
-const { MessageEmbed } = require("discord.js");
-const YouTubeAPI = require("simple-youtube-api");
-const { YOUTUBE_API_KEY, LOCALE } = require("../util/EvobotUtil");
-const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
-const i18n = require("i18n");
+/* eslint linebreak-style: 0 */
+const Command = require('../util/Command.js');
+const { MessageEmbed } = require('discord.js');
+const ytapi = require('simple-youtube-api'); 
+const { ytapikey } = require('../config.json');
+const youtube = new ytapi(ytapikey); 
+const handleVideo = require('../util/MusicHandling');
 
-i18n.setLocale(LOCALE);
-
-module.exports = {
-  name: "search",
-  description: i18n.__("search.description"),
-  async execute(message, args) {
-    if (!args.length)
-      return message
-        .reply(i18n.__mf("search.usageReply", { prefix: message.client.prefix, name: module.exports.name }))
-        .catch(console.error);
-    if (message.channel.activeCollector) return message.reply(i18n.__("search.errorAlreadyCollector"));
-    if (!message.member.voice.channel)
-      return message.reply(i18n.__("search.errorNotChannel")).catch(console.error);
-
-    const search = args.join(" ");
-
-    let resultsEmbed = new MessageEmbed()
-      .setTitle(i18n.__("search.resultEmbedTtile"))
-      .setDescription(i18n.__mf("search.resultEmbedDesc", { search: search }))
-      .setColor("#F8AA2A");
-
-    try {
-      const results = await youtube.searchVideos(search, 10);
-      results.map((video, index) => resultsEmbed.addField(video.shortURL, `${index + 1}. ${video.title}`));
-
-      let resultsMessage = await message.channel.send(resultsEmbed);
-
-      function filter(msg) {
-        const pattern = /^[0-9]{1,2}(\s*,\s*[0-9]{1,2})*$/;
-        return pattern.test(msg.content);
-      }
-
-      message.channel.activeCollector = true;
-      const response = await message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] });
-      const reply = response.first().content;
-
-      if (reply.includes(",")) {
-        let songs = reply.split(",").map((str) => str.trim());
-
-        for (let song of songs) {
-          await message.client.commands
-            .get("play")
-            .execute(message, [resultsEmbed.fields[parseInt(song) - 1].name]);
-        }
-      } else {
-        const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
-        message.client.commands.get("play").execute(message, [choice]);
-      }
-
-      message.channel.activeCollector = false;
-      resultsMessage.delete().catch(console.error);
-      response.first().delete().catch(console.error);
-    } catch (error) {
-      console.error(error);
-      message.channel.activeCollector = false;
-      message.reply(error.message).catch(console.error);
-    }
+class Search extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'pretraživanje',
+      description: 'Ova naredba omogućit će osobi da bira između 10 pjesama. ',
+      extended: 'Ova naredba omogućit će korisniku da bira između odabranih pjesama s frazom za pretraživanje, ograničavajući se na 10 rezultata',
+      usage: 'pretraživanje <song-name>',
+      cooldown: 2,
+      category: 'Music'
+    });
   }
-};
+
+  async run(message, args) {    
+    if (message.settings.djonly && !message.member.roles.some(c => c.name.toLowerCase() === message.settings.djrole.toLowerCase())) return message.client.embed('notDJ', message);
+    const voiceChannel = message.member.voiceChannel;
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!voiceChannel) return this.client.embed('nijeGasovnikanal', message);
+    if (!args[0]) return this.client.embed('noArgs', message);
+    if (!permissions.has('CONNECT')) return this.client.embed('noPerms-CONNECT', message);
+    if (!permissions.has('SPEAK')) return this.client.embed('noPerms-SPEAK', message);
+    let video;
+    try {
+      const videos = await youtube.searchVideos(args.join(' '), 10);
+      if (!videos.length) return this.client.embed('pjesma nije pronađena', message, args);
+      let index = 0;
+      const embed = new MessageEmbed()
+        .setAuthor('Song Selection')
+        .setDescription(`${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}`)
+        .setFooter('Navedite vrijednost za odabir jednog od rezultata pretraživanja u rasponu od 1 do 10'.)
+        .setColor(message.guild.me.roles.highest.color || 0x00AE86);
+      message.channel.send(embed);
+      const response = await message.channel.awaitMessages(msg2 => (msg2.content > 0 && msg2.content < 11) && msg2.author.id === message.author.id, {
+        max: 1,
+        time: 10000,
+        errors: ['time']
+      });
+      if (!response) return this.client.embed('invalidEntry', message);
+      const videoIndex = parseInt(response.first().content);
+      video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+    } catch (err) {
+      return this.client.embed('nemaNavedene pjesme', message);
+    }
+    return handleVideo(video, message, voiceChannel);
+  }
+}
+
+module.exports = Search;
